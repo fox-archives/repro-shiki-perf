1c9d662c56274c4fd65eaefa8901faade85a125c
Edwin Kofler <edwin@kofler.dev>
2024-11-28 14:04:42 -0800

    Initial commit

---
+++ main.js
@@ -0,0 +1,12 @@
+import Shiki from '@shikijs/markdown-it'
+import MarkdownIt from 'markdown-it'
+
+const md = MarkdownIt()
+
+md.use(await Shiki({
+  themes: {
+    light: 'vitesse-light',
+    dark: 'vitesse-dark',
+  }
+}))
+
